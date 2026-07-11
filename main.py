138b55ff6f5d56f11b7397c58953331ed13f87ef
Ivan_roomybear <ivanroomybear0409@gmail.com>
2026-07-12 01:14:44 +0700

    changed code in main.py

--- main.py
+++ main.py
@@ -9,3 +9,14 @@ print(gis_sum['amount_rub'].sum())
 clothes_pr = df[df['category'] == "Clothes"]
 clothes_me = clothes_pr.loc[clothes_pr['amount_rub'].idxmax()]
 print(clothes_me[['item', 'amount_rub']])
+
+# Task #3: Calculate the total sum (amount_rub) spent on the Food category where the payment_method was strictly Cash
+ctc_sum = df[(df['category'] == "Food") & (df['payment_method'] == "Cash")]
+sum1 = ctc_sum['amount_rub'].sum()
+print(sum1)
+
+# Task #4: Find the item name that appears the highest number of times in the dataset.
+print(df['item'].value_counts().idxmax())
+
+# Task #5: Calculate the average transaction value (amount_rub) for each unique category and return the category name with the highest average value.
+print(df.groupby('category')['amount_rub'].mean().idxmax())
